7d29c3a51d8c13789ad594ba2a834211308eb981
Kemisi <etolufashe@btinternet.com>
2023-02-07 15:13:46 +0000

    ticket number

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this script is only tested on ubuntu xenial
-
+##Extra package
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
@@ -13,12 +13,11 @@ apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
 usermod -aG docker ubuntu
-
+apt-get install vim git -y
 # run jenkins
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
 docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
-
 # show endpoint
 echo 'Jenkins installed'
 echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
